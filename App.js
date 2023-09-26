48a5b5ec09c6a16eb02ae5ed0b4d3f8c3f2655f4
Chelsea <chelsea@tripwiretech.com>
2023-09-26 20:45:18 +1300

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Network from "expo-network";
+import { useState, useEffect } from "react";
+
+// npx expo install expo-network
 
 export default function App() {
+  const [ipAddress, setIpAddress] = useState(undefined);
+  const [networkState, setNetworkState] = useState(undefined);
+
+  useEffect(() => {
+    const getIpAddress = async () => {
+      const ip = await Network.getIpAddressAsync();
+      setIpAddress(ip);
+    };
+    getIpAddress();
+
+    const getNetworkState = async () => {
+      const state = await Network.getNetworkStateAsync();
+      setNetworkState(JSON.stringify(state));
+    };
+    getNetworkState();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{ipAddress}</Text>
+      <Text>{networkState}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
